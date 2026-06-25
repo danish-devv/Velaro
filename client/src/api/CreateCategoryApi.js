5101fd2a1b4ee9c5d1b06ba48fa26418b1fed9f7
danish <danishmr835@gmail.com>
2026-06-25 18:00:31 +0500

    createcategoryapi file route fix

--- client/src/api/CreateCategoryApi.js
+++ client/src/api/CreateCategoryApi.js
@@ -1,7 +1,7 @@
 import api from "./axios";
 
  const createCategory = async (formData) => {
-  const response = await api.post("/categories", formData);
+  const response = await api.post("/category", formData);
 
   return response.data;
 };
