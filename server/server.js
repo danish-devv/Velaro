237423a9b3f0fc5f0fa621bc342222deee93308c
danish <danishmr835@gmail.com>
2026-06-25 13:09:08 +0500

    fix port typo

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import "./src/config/env.js"
 import app from "./src/app.js"
 import connectDB from "./src/config/db.js"
 
-app.listen(process.env.port,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("server is running successfuly")
 })
 
